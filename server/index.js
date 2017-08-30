c78d19559712e54645ee5c7ae030f737658ff6b7
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-30 15:53:58 -0600

    Created successful callback to Google servers for oauth, correctly logging user data

--- server/index.js
+++ server/index.js
@@ -12,8 +12,10 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
     })
 );
 
@@ -21,5 +23,9 @@ app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> {console.log(`Now docked on port ${PORT}!`)});
+app.listen(PORT, () => {
+    console.log(`Now docked on port ${PORT}!`)
+});
