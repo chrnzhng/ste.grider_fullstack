dc3e76d0702e5b25b9a39e6b7b0736001d380b48
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-30 13:20:23 -0600

    Added a config folder and googleoauth20

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth20').Strategy,
+    keys = require('./config/keys'),
 
-app.get('/', (req, res) => {
-    res.send({ Hey: 'Nick' });
-});
+
+    app = express();
+
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, ()=> {console.log(`Now docked on port ${PORT}!`)});
