c0ea396929abfcd4e1d422a9988518f537219dc0
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-31 14:52:21 -0700

    Hooked up app to connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const express = require('express'),
+    mongoose = require('mongoose'),
+    keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
