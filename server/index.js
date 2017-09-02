413a5a5b1eceb6e66cc7d72af37d2061666a024c
Jordan Zhang <chrn.zhng@gmail.com>
2017-09-02 14:18:42 -0700

    Added test route for authentication, verified working authentication

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express'),
     mongoose = require('mongoose'),
+    cookieSession = require('cookie-session'),
+    passport = require('passport');
     keys = require('./config/keys'),
-    cookieSession = require('./cookie-session');
     require('./models/User');
     require('./services/passport');
 
@@ -10,11 +11,19 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Now docked on port ${PORT}!`)
+    console.log(`Now listening on port ${PORT}!`)
 });
